020d1db9e6e098e0ce0a74f44e33bff281bbd23f
Georgy Smirnov <mr.hazzzard@gmail.com>
2022-10-05 16:37:47 +0300

    changed conftest.py

--- demoqa_registration_tests/conftest.py
+++ demoqa_registration_tests/conftest.py
@@ -6,7 +6,7 @@ from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
 from dotenv import load_dotenv
 
-from utils import attach
+import utils
 
 DEFAULT_BROWSER_VERSION = "100.0"
 
@@ -49,8 +49,8 @@ def setup_browser(request):
 
     yield browser
 
-    attach.add_html(browser)
-    attach.add_screenshot(browser)
-    attach.add_logs(browser)
-    attach.add_video(browser)
+    utils.attach.add_html(browser)
+    utils.attach.add_screenshot(browser)
+    utils.attach.add_logs(browser)
+    utils.attach.add_video(browser)
     browser.quit()
